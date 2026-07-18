c97eed28bc1e5e4a60c0b07eeb3a035e235d3a77
PTWEET <lockridgejames606@gmail.com>
2026-07-17 20:10:23 -0400

    Fix typo in function name from generated_schedule to generate_schedule

--- src/main.py
+++ src/main.py
@@ -72,7 +72,7 @@ def main():
         n = get_student_input(len(available))
         selected = get_user_selections(n, available)
 
-        schedule = generated_schedule(selected_raw)
+        schedule = generate_schedule(selected_raw)
 
         if not schedule:
             retry = handle_no_schedule(schedule)
